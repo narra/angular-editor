443db96b58bcb3ab8d36aaac0d477c49c40f33b2
michal@marigan.net <michal@marigan.net>
2021-11-09 17:28:49 +0100

    Project and library import / export support

--- src/app/components/libraries-settings/libraries-settings.component.ts
+++ src/app/components/libraries-settings/libraries-settings.component.ts
@@ -4,16 +4,18 @@
  * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Navigation} from '@app/models';
 import {narra} from '@narra/api';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {LibrariesNavigation} from '@app/navigation';
 import {forkJoin, Observable} from 'rxjs';
 import {RelationHelper, ScenarioHelper} from '@app/helpers';
-import {AuthService, BreadcrumbService, EventService} from '@app/services';
+import {AuthService, BreadcrumbService, EventService, MessageService} from '@app/services';
 import {EventType, RelationType} from '@app/enums';
 import cloneDeep from 'lodash/cloneDeep';
+import {saveAs} from 'file-saver';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
 
 @Component({
   selector: 'app-libraries-settings',
@@ -21,6 +23,7 @@ import cloneDeep from 'lodash/cloneDeep';
   styleUrls: ['./libraries-settings.component.scss']
 })
 export class LibrariesSettingsComponent implements OnInit {
+  @ViewChild('fileDropRef', {static: false}) fileDropEl: ElementRef;
 
   // public
   public navigation: Navigation;
@@ -30,6 +33,7 @@ export class LibrariesSettingsComponent implements OnInit {
   public changed: boolean;
   public clean: boolean;
   public delete: boolean;
+  public files: any[] = [];
   public relation: RelationType;
   public RelationType = RelationType;
 
@@ -40,12 +44,14 @@ export class LibrariesSettingsComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private eventService: EventService,
-    private libraryService: narra.LibraryService,
-    private scenarioService: narra.ScenarioService,
-    private projectService: narra.ProjectService,
+    private narraLibraryService: narra.LibraryService,
+    private narraScenarioService: narra.ScenarioService,
+    private narraProjectService: narra.ProjectService,
+    private narraReturnService: narra.ReturnService,
     private route: ActivatedRoute,
     private router: Router,
-    private breadcrumbsService: BreadcrumbService
+    private breadcrumbsService: BreadcrumbService,
+    private messageService: MessageService
   ) {
     this.loading = true;
     this.clean = false;
@@ -67,8 +73,8 @@ export class LibrariesSettingsComponent implements OnInit {
   private _load(): void {
     // process
     forkJoin([
-      this.libraryService.getLibrary(this.params.get('id')),
-      this.scenarioService.getScenarios()
+      this.narraLibraryService.getLibrary(this.params.get('id')),
+      this.narraScenarioService.getScenarios()
     ]).subscribe((responses) => {
       // get project
       this.library = responses[0].library;
@@ -97,7 +103,7 @@ export class LibrariesSettingsComponent implements OnInit {
     // close dialog
     this.clean = false;
     // clean library items
-    this.libraryService.cleanLibrary(this.library.id).subscribe((response) => {
+    this.narraLibraryService.cleanLibrary(this.library.id).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastLibraryEvent(this.library, EventType.library_cleaned);
       // reload data
@@ -109,7 +115,7 @@ export class LibrariesSettingsComponent implements OnInit {
     // set loading flag
     this.loading = true;
     // update project
-    this.libraryService.updateLibrary(this.library).subscribe((response) => {
+    this.narraLibraryService.updateLibrary(this.library).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastLibraryEvent(this.library, EventType.library_updated);
       // reload data
@@ -123,7 +129,7 @@ export class LibrariesSettingsComponent implements OnInit {
     // close dialog
     this.delete = false;
     // delete project
-    this.libraryService.deleteLibrary(this.library.id).subscribe((response) => {
+    this.narraLibraryService.deleteLibrary(this.library.id).subscribe((response) => {
       // redirect to dashboard
       if (response.id === this.library.id) {
         // broadcast event
@@ -134,6 +140,131 @@ export class LibrariesSettingsComponent implements OnInit {
         } else {
           this.router.navigate(['libraries']);
         }
+      }
+    });
+  }
+
+  /**
+   * handle file from browsing
+   */
+  public fileBrowseHandler(files): void {
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  private prepareFilesList(files: Array<any>): void {
+    for (const item of files) {
+      item.progress = 0;
+      item.error = false;
+      item.success = false;
+      if (!this.files.find((file) => file.name === item.name)) {
+        this.files.push(item);
+      }
+    }
+    this.fileDropEl.nativeElement.value = '';
+    this.uploadFile(0, true);
+  }
+
+  public uploadFile(index: number = 0, single: boolean = false, ignore: boolean = true): void {
+    // we're at the end
+    if (index === this.files.length) {
+      return;
+    }
+    // skipping finished ones and error if ignored
+    if (!this.files[index].success && !(ignore && this.files[index].error) && !this.files[index].uploading) {
+      // push uploading flag
+      this.files[index].uploading = true;
+      // upload
+      this.narraLibraryService.importLibrary(this.library.id, this.files[index]).subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            this.files[index].progress = 0;
+            this.files[index].error = false;
+            this.files[index].success = false;
+            break;
+          case HttpEventType.UploadProgress:
+            this.files[index].progress = Math.round(event.loaded / event.total * 100);
+            break;
+          case HttpEventType.Response:
+            if (event.body.event) {
+              this.files[index].success = true;
+              this.files[index].event = event.body.event;
+              // fire message
+              this.messageService.success('Library metadata import initiated.');
+            } else {
+              this.files[index].error = true;
+              // check for errors messages
+              if (event.body.errors) {
+                event.body.errors.forEach((error) => {
+                  this.messageService.error(error.message, error.trace);
+                });
+              }
+            }
+            // clean uploading flag
+            this.files[index].uploading = false;
+            // move on if wanted
+            if (!single) {
+              this.uploadFile(index + 1);
+            }
+        }
+      });
+    }
+    // if not single next
+    if (!single) {
+      this.uploadFile(index + 1);
+    }
+  }
+
+  public _exportLibrary(): void {
+    // export project
+    this.narraLibraryService.exportLibrary(this.library.id).subscribe((response) => {
+      // process return
+      this._processReturns([response.return], []);
+      // send message
+      this.messageService.success('Library metadata successfully exported');
+    });
+  }
+
+  private _processReturns(returns: narra.Return[], files: string[]): void {
+    // prepare objects
+    const joins = [];
+    // iterate over returns
+    returns.forEach((r) => {
+      // push into joins
+      joins.push(this.narraReturnService.getReturn(r.id));
+    });
+    // check for the rest once requests done
+    forkJoin<narra.Response<narra.Return, 'return'>>(joins).subscribe((responses) => {
+      // process responses
+      responses.forEach((responseee) => {
+        const id = responseee.return.id;
+        const url = responseee.return.url;
+        // process those with url
+        if (url != null) {
+          // remove from returns
+          returns = returns.filter((r) => r.id !== id);
+          // and into files
+          files.push(url);
+        }
+      });
+      // process when finished
+      if (!returns.length) {
+        // set wait flag to done and download files
+        files.forEach((file) => {
+          const parts = file.split('/');
+          const filename = parts[parts.length - 1];
+          saveAs(file, filename);
+        });
+        // send message
+        this.messageService.success('Files successfully downloaded');
+      } else {
+        // continue
+        setTimeout(() => {
+          this._processReturns(returns, files);
+        }, 1000);
       }
     });
   }
--- src/app/components/projects-settings/projects-settings.component.ts
+++ src/app/components/projects-settings/projects-settings.component.ts
@@ -4,16 +4,18 @@
  * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Navigation} from '@app/models';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import {ProjectsNavigation} from '@app/navigation';
 import {forkJoin} from 'rxjs';
 import {RelationHelper, ScenarioHelper} from '@app/helpers';
 import {narra} from '@narra/api';
-import {AuthService, BreadcrumbService, EventService} from '@app/services';
+import {AuthService, BreadcrumbService, EventService, MessageService} from '@app/services';
 import {EventType, RelationType} from '@app/enums';
 import cloneDeep from 'lodash/cloneDeep';
+import {saveAs} from 'file-saver';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
 
 @Component({
   selector: 'app-projects-settings',
@@ -21,6 +23,7 @@ import cloneDeep from 'lodash/cloneDeep';
   styleUrls: ['./projects-settings.component.scss']
 })
 export class ProjectsSettingsComponent implements OnInit {
+  @ViewChild('fileDropRef', {static: false}) fileDropEl: ElementRef;
 
   // public
   public navigation: Navigation;
@@ -29,6 +32,7 @@ export class ProjectsSettingsComponent implements OnInit {
   public scenarios: narra.Scenario[];
   public changed: boolean;
   public delete: boolean;
+  public files: any[] = [];
   public relation: RelationType;
   public RelationType = RelationType;
 
@@ -39,11 +43,13 @@ export class ProjectsSettingsComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private eventService: EventService,
-    private projectService: narra.ProjectService,
-    private scenarioService: narra.ScenarioService,
+    private narraProjectService: narra.ProjectService,
+    private narraScenarioService: narra.ScenarioService,
+    private narraReturnService: narra.ReturnService,
     private route: ActivatedRoute,
     private router: Router,
-    private breadcrumbsService: BreadcrumbService
+    private breadcrumbsService: BreadcrumbService,
+    private messageService: MessageService
   ) {
     this.loading = true;
     this.delete = false;
@@ -63,8 +69,8 @@ export class ProjectsSettingsComponent implements OnInit {
 
   private _load(): void {
     forkJoin([
-      this.projectService.getProject(this.params.get('name')),
-      this.scenarioService.getScenarios()
+      this.narraProjectService.getProject(this.params.get('name')),
+      this.narraScenarioService.getScenarios()
     ]).subscribe(results => {
       // get project
       this.project = results[0].project;
@@ -91,7 +97,7 @@ export class ProjectsSettingsComponent implements OnInit {
     // set loading flag
     this.loading = true;
     // update project
-    this.projectService.updateProject(this.project).subscribe(project => {
+    this.narraProjectService.updateProject(this.project).subscribe(project => {
       // broadcast event
       this.eventService.broadcastProjectEvent(this.project, EventType.project_updated);
       // reload data
@@ -105,13 +111,138 @@ export class ProjectsSettingsComponent implements OnInit {
     // close dialog
     this.delete = false;
     // delete project
-    this.projectService.deleteProject(this.project.id).subscribe((response) => {
+    this.narraProjectService.deleteProject(this.project.id).subscribe((response) => {
       // redirect to dashboard
       if (response.id === this.project.id) {
         // broadcast event
         this.eventService.broadcastProjectEvent(this.project, EventType.project_deleted);
         // redirect
         this.router.navigate(['projects']);
+      }
+    });
+  }
+
+  /**
+   * handle file from browsing
+   */
+  public fileBrowseHandler(files): void {
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  private prepareFilesList(files: Array<any>): void {
+    for (const item of files) {
+      item.progress = 0;
+      item.error = false;
+      item.success = false;
+      if (!this.files.find((file) => file.name === item.name)) {
+        this.files.push(item);
+      }
+    }
+    this.fileDropEl.nativeElement.value = '';
+    this.uploadFile(0, true);
+  }
+
+  public uploadFile(index: number = 0, single: boolean = false, ignore: boolean = true): void {
+    // we're at the end
+    if (index === this.files.length) {
+      return;
+    }
+    // skipping finished ones and error if ignored
+    if (!this.files[index].success && !(ignore && this.files[index].error) && !this.files[index].uploading) {
+      // push uploading flag
+      this.files[index].uploading = true;
+      // upload
+      this.narraProjectService.importProject(this.project.id, this.files[index]).subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            this.files[index].progress = 0;
+            this.files[index].error = false;
+            this.files[index].success = false;
+            break;
+          case HttpEventType.UploadProgress:
+            this.files[index].progress = Math.round(event.loaded / event.total * 100);
+            break;
+          case HttpEventType.Response:
+            if (event.body.event) {
+              this.files[index].success = true;
+              this.files[index].event = event.body.event;
+              // fire message
+              this.messageService.success('Project\'s metadata import initiated.');
+            } else {
+              this.files[index].error = true;
+              // check for errors messages
+              if (event.body.errors) {
+                event.body.errors.forEach((error) => {
+                  this.messageService.error(error.message, error.trace);
+                });
+              }
+            }
+            // clean uploading flag
+            this.files[index].uploading = false;
+            // move on if wanted
+            if (!single) {
+              this.uploadFile(index + 1);
+            }
+        }
+      });
+    }
+    // if not single next
+    if (!single) {
+      this.uploadFile(index + 1);
+    }
+  }
+
+  public _exportProject(): void {
+    // export project
+    this.narraProjectService.exportProject(this.project.id).subscribe((response) => {
+      // process return
+      this._processReturns([response.return], []);
+      // send message
+      this.messageService.success('Project\'s metadata successfully exported');
+    });
+  }
+
+  private _processReturns(returns: narra.Return[], files: string[]): void {
+    // prepare objects
+    const joins = [];
+    // iterate over returns
+    returns.forEach((r) => {
+      // push into joins
+      joins.push(this.narraReturnService.getReturn(r.id));
+    });
+    // check for the rest once requests done
+    forkJoin<narra.Response<narra.Return, 'return'>>(joins).subscribe((responses) => {
+      // process responses
+      responses.forEach((responseee) => {
+        const id = responseee.return.id;
+        const url = responseee.return.url;
+        // process those with url
+        if (url != null) {
+          // remove from returns
+          returns = returns.filter((r) => r.id !== id);
+          // and into files
+          files.push(url);
+        }
+      });
+      // process when finished
+      if (!returns.length) {
+        // set wait flag to done and download files
+        files.forEach((file) => {
+          const parts = file.split('/');
+          const filename = parts[parts.length - 1];
+          saveAs(file, filename);
+        });
+        // send message
+        this.messageService.success('Files successfully downloaded');
+      } else {
+        // continue
+        setTimeout(() => {
+          this._processReturns(returns, files);
+        }, 1000);
       }
     });
   }
