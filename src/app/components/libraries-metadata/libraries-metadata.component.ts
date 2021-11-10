447c74e068d40ed0341377e664e7fe44751c4685
michal@marigan.net <michal@marigan.net>
2021-11-10 23:39:55 +0100

    Project and library copy implemented

--- src/app/components/libraries-metadata/libraries-metadata.component.ts
+++ src/app/components/libraries-metadata/libraries-metadata.component.ts
@@ -4,16 +4,18 @@
  * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {Navigation} from '@app/models';
 import {narra} from '@narra/api';
 import {ActivatedRoute, ParamMap} from '@angular/router';
-import {Subscription} from 'rxjs';
-import {AddService, AuthService, BreadcrumbService, EventService} from '@app/services';
+import {forkJoin, Subscription} from 'rxjs';
+import {AddService, AuthService, BreadcrumbService, EventService, MessageService} from '@app/services';
 import {LibrariesNavigation} from '@app/navigation';
 import {EventType, RelationType} from '@app/enums';
 import {RelationHelper} from '@app/helpers';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-libraries-metadata',
@@ -22,15 +24,21 @@ import {RelationHelper} from '@app/helpers';
 })
 export class LibrariesMetadataComponent implements OnInit {
   @ViewChild('formAdd') formAdd: FormGroup;
+  @ViewChild('formCopy') formCopy: FormGroup;
+  @ViewChild('fileDropRef', {static: false}) fileDropEl: ElementRef;
 
   // public
   public navigation: Navigation;
   public loading: boolean;
   public library: narra.Library;
+  public destination: narra.Library;
+  public libraries: narra.Library[];
   public params: ParamMap;
   public relation: RelationType;
   public openAddDialog: boolean;
+  public openCopyDialog: boolean;
   public newMeta: Pick<narra.Meta, 'name' | 'value'>;
+  public files: any[] = [];
   public RelationType = RelationType;
   public RelationHelper = RelationHelper;
 
@@ -38,15 +46,18 @@ export class LibrariesMetadataComponent implements OnInit {
   private subscription: Subscription;
 
   constructor(
-    private libraryService: narra.LibraryService,
+    private narraLibraryService: narra.LibraryService,
+    private narraReturnService: narra.ReturnService,
     private authService: AuthService,
     private addService: AddService,
     private eventService: EventService,
     private route: ActivatedRoute,
-    private breadcrumbsService: BreadcrumbService
+    private breadcrumbsService: BreadcrumbService,
+    private messageService: MessageService
   ) {
     this.loading = true;
     this.openAddDialog = false;
+    this.openCopyDialog = false;
     this.newMeta = this.emptyMetadata();
     this.relation = RelationType.owned;
   }
@@ -77,9 +88,18 @@ export class LibrariesMetadataComponent implements OnInit {
   }
 
   private _load(): void {
-    this.libraryService.getLibrary(this.params.get('id')).subscribe((response) => {
-      // get project
-      this.library = response.library;
+    // prepare objects
+    const joins = [this.narraLibraryService.getLibrary(this.params.get('id')), this.narraLibraryService.getLibraries()];
+    // process
+    forkJoin<any>(joins).subscribe((responses) => {
+      // process libraries
+      this.libraries = (responses[1] as narra.Response<narra.Library[], 'libraries'>).libraries.filter((library) => library.id !== this.params.get('id'));
+      // select destination
+      if (this.libraries.length) {
+        this.destination = this.libraries[0];
+      }
+      // process library
+      this.library = (responses[0] as narra.Response<narra.Library, 'library'>).library;
       // breadcrumbs
       this.breadcrumbsService.updateLibrary(this.library.id, this.library.name);
       // relation
@@ -88,9 +108,15 @@ export class LibrariesMetadataComponent implements OnInit {
       this.newMeta = this.emptyMetadata();
       // reset form
       this.formAdd.reset();
+      // clean files
+      this.files = [];
       // loading done
       this.loading = false;
     });
+  }
+
+  public test(test: any) {
+    console.log(test.control.value);
   }
 
   public emptyMetadata(): Pick<narra.Meta, 'name' | 'value'> {
@@ -105,7 +131,7 @@ export class LibrariesMetadataComponent implements OnInit {
     // set loading flag
     this.loading = true;
     // update project
-    this.libraryService.updateLibraryMeta(this.library.id, meta).subscribe((response) => {
+    this.narraLibraryService.updateLibraryMeta(this.library.id, meta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastLibraryEvent(this.library, EventType.library_updated);
       // reload data
@@ -119,7 +145,7 @@ export class LibrariesMetadataComponent implements OnInit {
     // set loading flag
     this.loading = true;
     // update project
-    this.libraryService.addLibraryMeta(this.library.id, this.newMeta).subscribe((response) => {
+    this.narraLibraryService.addLibraryMeta(this.library.id, this.newMeta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastLibraryEvent(this.library, EventType.library_updated);
       // reload data
@@ -131,11 +157,152 @@ export class LibrariesMetadataComponent implements OnInit {
     // set loading flag
     this.loading = true;
     // update project
-    this.libraryService.deleteLibraryMeta(this.library.id, meta).subscribe((response) => {
+    this.narraLibraryService.deleteLibraryMeta(this.library.id, meta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastLibraryEvent(this.library, EventType.library_updated);
       // reload data
       this._load();
+    });
+  }
+
+  public _exportMeta(): void {
+    // export project
+    this.narraLibraryService.exportLibrary(this.library.id).subscribe((response) => {
+      // process return
+      this._processReturns([response.return], []);
+      // send message
+      this.messageService.success('Library metadata successfully exported');
+    });
+  }
+
+  public _copyMeta(): void {
+    // copy library
+    this.narraLibraryService.copyLibrary(this.library.id, this.destination.id).subscribe((response) => {
+      // send message
+      this.messageService.success('Library metadata copy process successfully initiated');
+      // close dialog
+      this.openCopyDialog = false;
+    });
+  }
+
+  /**
+   * handle file from browsing
+   */
+  public fileBrowseHandler(files): void {
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  private prepareFilesList(files: Array<any>): void {
+    for (const item of files) {
+      item.progress = 0;
+      item.error = false;
+      item.success = false;
+      if (!this.files.find((file) => file.name === item.name)) {
+        this.files.push(item);
+      }
+    }
+    this.fileDropEl.nativeElement.value = '';
+    this.uploadFile(0, true);
+  }
+
+  public uploadFile(index: number = 0, single: boolean = false, ignore: boolean = true): void {
+    // we're at the end
+    if (index === this.files.length) {
+      return;
+    }
+    // skipping finished ones and error if ignored
+    if (!this.files[index].success && !(ignore && this.files[index].error) && !this.files[index].uploading) {
+      // push uploading flag
+      this.files[index].uploading = true;
+      // set loading flag
+      this.loading = true;
+      // upload
+      this.narraLibraryService.importLibrary(this.library.id, this.files[index]).subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            this.files[index].progress = 0;
+            this.files[index].error = false;
+            this.files[index].success = false;
+            break;
+          case HttpEventType.UploadProgress:
+            this.files[index].progress = Math.round(event.loaded / event.total * 100);
+            break;
+          case HttpEventType.Response:
+            if (event.body.event) {
+              this.files[index].success = true;
+              this.files[index].event = event.body.event;
+              // fire message
+              this.messageService.success('Library metadata import initiated.');
+              // wait and reload page
+              setTimeout(() => {
+                this._load();
+              }, 2000);
+            } else {
+              this.files[index].error = true;
+              // check for errors messages
+              if (event.body.errors) {
+                event.body.errors.forEach((error) => {
+                  this.messageService.error(error.message, error.trace);
+                });
+              }
+            }
+            // clean uploading flag
+            this.files[index].uploading = false;
+            // move on if wanted
+            if (!single) {
+              this.uploadFile(index + 1);
+            }
+        }
+      });
+    }
+    // if not single next
+    if (!single) {
+      this.uploadFile(index + 1);
+    }
+  }
+
+  private _processReturns(returns: narra.Return[], files: string[]): void {
+    // prepare objects
+    const joins = [];
+    // iterate over returns
+    returns.forEach((r) => {
+      // push into joins
+      joins.push(this.narraReturnService.getReturn(r.id));
+    });
+    // check for the rest once requests done
+    forkJoin<narra.Response<narra.Return, 'return'>>(joins).subscribe((responses) => {
+      // process responses
+      responses.forEach((responseee) => {
+        const id = responseee.return.id;
+        const url = responseee.return.url;
+        // process those with url
+        if (url != null) {
+          // remove from returns
+          returns = returns.filter((r) => r.id !== id);
+          // and into files
+          files.push(url);
+        }
+      });
+      // process when finished
+      if (!returns.length) {
+        // set wait flag to done and download files
+        files.forEach((file) => {
+          const parts = file.split('/');
+          const filename = parts[parts.length - 1];
+          saveAs(file, filename);
+        });
+        // send message
+        this.messageService.success('Files successfully downloaded');
+      } else {
+        // continue
+        setTimeout(() => {
+          this._processReturns(returns, files);
+        }, 1000);
+      }
     });
   }
 }
--- src/app/components/projects-metadata/projects-metadata.component.ts
+++ src/app/components/projects-metadata/projects-metadata.component.ts
@@ -4,16 +4,18 @@
  * GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Navigation} from '@app/models';
 import {narra} from '@narra/api';
 import {ActivatedRoute, ParamMap} from '@angular/router';
-import {Subscription} from 'rxjs';
+import {forkJoin, Subscription} from 'rxjs';
 import {EventType, RelationType} from '@app/enums';
 import {RelationHelper} from '@app/helpers';
-import {AddService, AuthService, BreadcrumbService, EventService} from '@app/services';
+import {AddService, AuthService, BreadcrumbService, EventService, MessageService} from '@app/services';
 import {ProjectsNavigation} from '@app/navigation';
 import {FormGroup} from '@angular/forms';
+import {HttpEvent, HttpEventType} from '@angular/common/http';
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-projects-metadata',
@@ -22,15 +24,21 @@ import {FormGroup} from '@angular/forms';
 })
 export class ProjectsMetadataComponent implements OnInit, OnDestroy {
   @ViewChild('formAdd') formAdd: FormGroup;
+  @ViewChild('formCopy') formCopy: FormGroup;
+  @ViewChild('fileDropRef', {static: false}) fileDropEl: ElementRef;
 
   // public
   public navigation: Navigation;
   public loading: boolean;
   public project: narra.Project;
+  public destination: narra.Project;
+  public projects: narra.Project[];
   public params: ParamMap;
   public relation: RelationType;
   public openAddDialog: boolean;
+  public openCopyDialog: boolean;
   public newMeta: Pick<narra.Meta, 'name' | 'value'>;
+  public files: any[] = [];
   public RelationType = RelationType;
   public RelationHelper = RelationHelper;
 
@@ -38,15 +46,18 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
   private subscription: Subscription;
 
   constructor(
-    private projectService: narra.ProjectService,
+    private narraProjectService: narra.ProjectService,
+    private narraReturnService: narra.ReturnService,
     private authService: AuthService,
     private addService: AddService,
     private eventService: EventService,
     private route: ActivatedRoute,
-    private breadcrumbsService: BreadcrumbService
+    private breadcrumbsService: BreadcrumbService,
+    private messageService: MessageService
   ) {
     this.loading = true;
     this.openAddDialog = false;
+    this.openCopyDialog = false;
     this.newMeta = this.emptyMetadata();
     this.relation = RelationType.owned;
   }
@@ -77,9 +88,18 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
   }
 
   private _load(): void {
-    this.projectService.getProject(this.params.get('name')).subscribe((response) => {
+    // prepare objects
+    const joins = [this.narraProjectService.getProject(this.params.get('name')), this.narraProjectService.getProjects()];
+    // process
+    forkJoin<any>(joins).subscribe((responses) => {
+      // process libraries
+      this.projects = (responses[1] as narra.Response<narra.Project[], 'projects'>).projects.filter((project) => project.id !== this.params.get('name'));
+      // select destination
+      if (this.projects.length) {
+        this.destination = this.projects[0];
+      }
       // get project
-      this.project = response.project;
+      this.project = (responses[0] as narra.Response<narra.Project, 'project'>).project;
       // breadcrumbs
       this.breadcrumbsService.updateProject(this.project.id, this.project.name);
       // relation
@@ -88,6 +108,8 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
       this.newMeta = this.emptyMetadata();
       // reset form
       this.formAdd.reset();
+      // clean files
+      this.files = [];
       // loading done
       this.loading = false;
     });
@@ -105,7 +127,7 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
     // set loading flag
     this.loading = true;
     // update project
-    this.projectService.updateProjectMeta(this.project.id, meta).subscribe((response) => {
+    this.narraProjectService.updateProjectMeta(this.project.id, meta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastProjectEvent(this.project, EventType.project_updated);
       // reload data
@@ -119,7 +141,7 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
     // set loading flag
     this.loading = true;
     // update project
-    this.projectService.addProjectMeta(this.project.id, this.newMeta).subscribe((response) => {
+    this.narraProjectService.addProjectMeta(this.project.id, this.newMeta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastProjectEvent(this.project, EventType.project_updated);
       // reload data
@@ -131,11 +153,152 @@ export class ProjectsMetadataComponent implements OnInit, OnDestroy {
     // set loading flag
     this.loading = true;
     // update project
-    this.projectService.deleteProjectMeta(this.project.id, meta).subscribe((response) => {
+    this.narraProjectService.deleteProjectMeta(this.project.id, meta).subscribe((response) => {
       // broadcast event
       this.eventService.broadcastProjectEvent(this.project, EventType.project_updated);
       // reload data
       this._load();
+    });
+  }
+
+  public _exportMeta(): void {
+    // export project
+    this.narraProjectService.exportProject(this.project.id).subscribe((response) => {
+      // process return
+      this._processReturns([response.return], []);
+      // send message
+      this.messageService.success('Project\'s metadata successfully exported');
+    });
+  }
+
+  public _copyMeta(): void {
+    // copy library
+    this.narraProjectService.copyProject(this.project.id, this.destination.id).subscribe((response) => {
+      // send message
+      this.messageService.success('Project metadata copy process successfully initiated');
+      // close dialog
+      this.openCopyDialog = false;
+    });
+  }
+
+  /**
+   * handle file from browsing
+   */
+  public fileBrowseHandler(files): void {
+    this.prepareFilesList(files);
+  }
+
+  /**
+   * Convert Files list to normal array list
+   * @param files (Files List)
+   */
+  private prepareFilesList(files: Array<any>): void {
+    for (const item of files) {
+      item.progress = 0;
+      item.error = false;
+      item.success = false;
+      if (!this.files.find((file) => file.name === item.name)) {
+        this.files.push(item);
+      }
+    }
+    this.fileDropEl.nativeElement.value = '';
+    this.uploadFile(0, true);
+  }
+
+  public uploadFile(index: number = 0, single: boolean = false, ignore: boolean = true): void {
+    // we're at the end
+    if (index === this.files.length) {
+      return;
+    }
+    // skipping finished ones and error if ignored
+    if (!this.files[index].success && !(ignore && this.files[index].error) && !this.files[index].uploading) {
+      // push uploading flag
+      this.files[index].uploading = true;
+      // set loading flag
+      this.loading = true;
+      // upload
+      this.narraProjectService.importProject(this.project.id, this.files[index]).subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            this.files[index].progress = 0;
+            this.files[index].error = false;
+            this.files[index].success = false;
+            break;
+          case HttpEventType.UploadProgress:
+            this.files[index].progress = Math.round(event.loaded / event.total * 100);
+            break;
+          case HttpEventType.Response:
+            if (event.body.event) {
+              this.files[index].success = true;
+              this.files[index].event = event.body.event;
+              // fire message
+              this.messageService.success('Project\'s metadata import initiated.');
+              // wait and reload page
+              setTimeout(() => {
+                this._load();
+              }, 2000);
+            } else {
+              this.files[index].error = true;
+              // check for errors messages
+              if (event.body.errors) {
+                event.body.errors.forEach((error) => {
+                  this.messageService.error(error.message, error.trace);
+                });
+              }
+            }
+            // clean uploading flag
+            this.files[index].uploading = false;
+            // move on if wanted
+            if (!single) {
+              this.uploadFile(index + 1);
+            }
+        }
+      });
+    }
+    // if not single next
+    if (!single) {
+      this.uploadFile(index + 1);
+    }
+  }
+
+  private _processReturns(returns: narra.Return[], files: string[]): void {
+    // prepare objects
+    const joins = [];
+    // iterate over returns
+    returns.forEach((r) => {
+      // push into joins
+      joins.push(this.narraReturnService.getReturn(r.id));
+    });
+    // check for the rest once requests done
+    forkJoin<narra.Response<narra.Return, 'return'>>(joins).subscribe((responses) => {
+      // process responses
+      responses.forEach((responseee) => {
+        const id = responseee.return.id;
+        const url = responseee.return.url;
+        // process those with url
+        if (url != null) {
+          // remove from returns
+          returns = returns.filter((r) => r.id !== id);
+          // and into files
+          files.push(url);
+        }
+      });
+      // process when finished
+      if (!returns.length) {
+        // set wait flag to done and download files
+        files.forEach((file) => {
+          const parts = file.split('/');
+          const filename = parts[parts.length - 1];
+          saveAs(file, filename);
+        });
+        // send message
+        this.messageService.success('Files successfully downloaded');
+      } else {
+        // continue
+        setTimeout(() => {
+          this._processReturns(returns, files);
+        }, 1000);
+      }
     });
   }
 }
